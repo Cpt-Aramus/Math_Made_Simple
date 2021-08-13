490ce28b7a4c81658199316824c36489719d7c4c
Christian <christian-langer96@t-online.de>
2021-08-13 22:24:17 +0200

    Commit 2 - Implemented GridView Layout in list_item_main_menu.xml

--- app/src/main/java/MainMenu/MainMenu.java
+++ app/src/main/java/MainMenu/MainMenu.java
@@ -3,14 +3,24 @@ package MainMenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
+
 import com.example.mathmadesimple.R;
 
 public class MainMenu extends AppCompatActivity {
+
+    GridView gridViewMainMenu;
+
+    String[] itemsMainMenu = {"Geomotry", "Simple Math Operations", "Graphs", "Functions"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        gridViewMainMenu = findViewById(R.id.grid_view_main_menu);
+
     }
+
+
 }
